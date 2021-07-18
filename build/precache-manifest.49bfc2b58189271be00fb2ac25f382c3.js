be2b08e18f368d12b7fd209ea930c4befc3de426
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-07-17 19:06:35 -0500

    build

---
+++ build/precache-manifest.49bfc2b58189271be00fb2ac25f382c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32d3d270f92faf7b49f0d806952c4c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e85d9b9958d8be25d109",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "99a7ef07f85cbac48246",
+    "url": "/static/js/2.0eacd543.chunk.js"
+  },
+  {
+    "revision": "fde8a65b8d307872c8c44f6de8ee07de",
+    "url": "/static/js/2.0eacd543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e85d9b9958d8be25d109",
+    "url": "/static/js/main.0ff68273.chunk.js"
+  },
+  {
+    "revision": "dfa477a8b638e329d13d",
+    "url": "/static/js/runtime-main.dc62470f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
